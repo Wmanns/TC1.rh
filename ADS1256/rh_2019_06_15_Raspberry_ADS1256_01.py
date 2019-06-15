7e246076ddead768d1720068b3b68f69e0877ce6
wmanns <wmanns@freenet.de>
2019-06-16 01:40:16 +0200

    Update rh_2019_06_15_Raspberry_ADS1256_01.py
    Improved commentary

--- ADS1256/rh_2019_06_15_Raspberry_ADS1256_01.py
+++ ADS1256/rh_2019_06_15_Raspberry_ADS1256_01.py
@@ -2,10 +2,10 @@
 # -*- coding: utf-8 -*-
 """PiPyADC:
 Hardware: Waveshare ADS1256 board interfaced to the Raspberry Pi 3
-Ulrich Lukas 2017-03-10
+Ulrich Lukas 2017-03-10  (https://github.com/ul-gh/PiPyADC)
 wm           2019-06-16
 
-wm: Thanks to Ulrich! (https://github.com/ul-gh/PiPyADC)
+wm: Thanks to Ulrich!
 Use his library to do measurements with the 'Waveshare high precision AD/DA Board'.
 (I did not succeed to use this board with other libraries).
 In this slightly modified program only one channel is used for AD conversion.
